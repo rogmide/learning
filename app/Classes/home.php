543f6d8fbd20978e53070b470c349b2f55d95f73
Roger Delgado <rogmide@gmail.com>
2023-09-12 08:39:47 -0400

    moving to laravel

--- app/Classes/home.php
+++ app/Classes/home.php
@@ -6,6 +6,19 @@ class Home
 {
     public static function index()
     {
-        return 'Home';
+
+        echo '<pre>';
+        var_dump($_REQUEST);
+        echo '</pre>';
+
+        echo '<pre>';
+        var_dump($_GET);
+        echo '</pre>';
+
+        echo '<pre>';
+        var_dump($_POST);
+        echo '</pre>';
+
+        return '<form action="/?foo=bar&amount=250" method="post"><lable>Amount</lable><input type="text" name="amount" />';
     }
 }
